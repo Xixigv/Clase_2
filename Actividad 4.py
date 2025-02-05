398f49ea6b889576fa34c87f9c700ec9f0031575
Xixigv <100481289+Xixigv@users.noreply.github.com>
2025-02-05 16:25:38 -0600

    Add files via upload

---
+++ Actividad 4.py
@@ -0,0 +1,52 @@
+#actividad 4
+# DEFINIR LAS VARIABLES CON LOS SIGUIENTES VALORES
+
+#valores operacion 1
+a = 8
+b = 10
+operacion = (5*a)+(6*b)
+print("el resultado de la operacion es:",operacion)
+print()
+
+# valores operacion 2
+A = 10
+B = 20
+C = 30
+operacion_2 = (A+B+C)/3
+print("el resultado de la operacion es:",operacion_2)
+print()
+#oython convierte los datos en una division automaticamente a un dato flotante
+
+# valores de operacion 3
+x = 50
+y = 10
+z = 15
+operacion_3= (3*x+6*y)/(7*z)
+print("el resultado de la operacion es:",operacion_3)
+print()
+
+# valores de operacion 4
+m = 2
+n = 10
+x2 = 4
+operacion_4 = (m**3)+(n**x2)
+print("el resultado de la operacion es:",operacion_4)
+print()
+
+# valores operacion 5
+M = 15
+N = 10
+O = 5
+operacion_5 = (M/3)-(N/5)+(O**(1/3))
+print("el resultado de la operacion es:",operacion_5)
+#Se corrigió
+# "Únicamente en la última expresión,
+#  en el segundo término,
+# invertiste el numerador por el denominador."
+
+#para imprimmir con la misma variable se puede realizar de las siguientes maneras
+#   print(f"texto" {identificador}
+#   print("texto", identificador
+#   print("texto{}", format(identificador)
+
+
